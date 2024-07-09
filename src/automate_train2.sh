6810bd9482d056544aa60601441211e8d267aa99
Reese Kneeland <reesekneeland@gmail.com>
2024-07-09 08:17:20 -0500

    Adding first version of ip adapter in training and inference code, updating training automation scripts, adding custom IPAdapter classes in models.py

--- src/automate_train2.sh
+++ src/automate_train2.sh
@@ -1,20 +1,30 @@
 
 jupyter nbconvert Train.ipynb --to python
+jupyter nbconvert recon_inference_mi.ipynb --to python
 export NUM_GPUS=1  # Set to equal gres=gpu:#!
-export BATCH_SIZE=10 # 21 for multisubject / 24 for singlesubject (orig. paper used 42 for multisubject / 24 for singlesubject)
+export BATCH_SIZE=63 # 21 for multisubject / 24 for singlesubject (orig. paper used 42 for multisubject / 24 for singlesubject)
 export GLOBAL_BATCH_SIZE=$((BATCH_SIZE * NUM_GPUS))
-export CUDA_VISIBLE_DEVICES="2"
+export CUDA_VISIBLE_DEVICES="3"
 
-# singlesubject finetuning
-model_name="pretrained_subj09irf_40sess_hypatia_no_blurry_noirfpt_all"
-echo model_name=${model_name}
-python Train.py --data_path=../dataset --cache_dir=../cache --model_name=${model_name} --no-multi_subject --subj=9 --batch_size=${BATCH_SIZE} --max_lr=6e-5 --mixup_pct=.33 --num_epochs=150 --use_prior --prior_scale=30 --clip_scale=1 --blur_scale=.5 --no-use_image_aug --n_blocks=4 --hidden_dim=1024 --num_sessions=40 --ckpt_interval=999 --ckpt_saving --wandb_log --multisubject_ckpt=../train_logs/multisubject_subj01_hypatia_no_blurry2 --mode=all --no-blurry_recon #--resume_from_ckpt
+for subj in 5 7; do
+    pretrain_model_name="multisubject_subj0${subj}_hypatia_ip_adapter"
+    echo model_name=${pretrain_model_name}
+    python Train.py --data_path=../dataset --cache_dir=../cache --model_name=${pretrain_model_name} --multi_subject --subj=$subj --batch_size=${BATCH_SIZE} --max_lr=6e-5 --mixup_pct=.33 --num_epochs=150 --use_prior --prior_scale=30 --clip_scale=1 --blur_scale=.5 --no-use_image_aug --n_blocks=4 --hidden_dim=1024 --num_sessions=40 --ckpt_interval=999 --ckpt_saving --wandb_log #--no-blurry_recon
 
-pretrain_model_name="multisubject_subj09irf_hypatia_imageryrf_all_no_blurry"
-echo model_name=${pretrain_model_name}
-python Train.py --data_path=../dataset --cache_dir=../cache --model_name=${pretrain_model_name} --multi_subject --subj=9 --batch_size=${BATCH_SIZE} --max_lr=6e-5 --mixup_pct=.33 --num_epochs=150 --use_prior --prior_scale=30 --clip_scale=1 --blur_scale=.5 --no-use_image_aug --n_blocks=4 --hidden_dim=1024 --num_sessions=40 --ckpt_interval=999 --ckpt_saving --wandb_log --train_imageryrf --mode=all --no-blurry_recon
+    # singlesubject finetuning
+    model_name="pretrained_subj0${subj}_40sess_hypatia_ip_adapter"
+    echo model_name=${model_name}
+    python Train.py --data_path=../dataset --cache_dir=../cache --model_name=${model_name} --no-multi_subject --subj=$subj --batch_size=${BATCH_SIZE} --max_lr=6e-5 --mixup_pct=.33 --num_epochs=150 --use_prior --prior_scale=30 --clip_scale=1 --blur_scale=.5 --no-use_image_aug --n_blocks=4 --hidden_dim=1024 --num_sessions=40 --ckpt_interval=999 --ckpt_saving --wandb_log --multisubject_ckpt=../train_logs/${pretrain_model_name} #--no-blurry_recon #--resume_from_ckpt
 
-# singlesubject finetuning
-model_name="pretrained_subj09irf_40sess_hypatia_imageryrf_all_no_blurry"
-echo model_name=${model_name}
-python Train.py --data_path=../dataset --cache_dir=../cache --model_name=${model_name} --no-multi_subject --subj=9 --batch_size=${BATCH_SIZE} --max_lr=6e-5 --mixup_pct=.33 --num_epochs=150 --use_prior --prior_scale=30 --clip_scale=1 --blur_scale=.5 --no-use_image_aug --n_blocks=4 --hidden_dim=1024 --num_sessions=40 --ckpt_interval=999 --ckpt_saving --wandb_log --multisubject_ckpt=../train_logs/${pretrain_model_name} --mode=all --no-blurry_recon #--resume_from_ckpt
+    for mode in "imagery" "vision"; do
+        python recon_inference_mi.py \
+            --model_name $model_name \
+            --subj $subj \
+            --mode $mode \
+            --cache_dir ../cache \
+            --data_path ../dataset \
+            --hidden_dim 1024 \
+            --n_blocks 4
+
+        done
+    done
--- src/models.py
+++ src/models.py
@@ -8,8 +8,14 @@ import clip
 from functools import partial
 import random
 import json
+from safetensors import safe_open
 from tqdm import tqdm
+from typing import List
 import utils
+from ip_adapter import IPAdapter
+from ip_adapter.resampler import Resampler
+from ip_adapter.utils import get_generator
+from transformers import CLIPImageProcessor, CLIPVisionModelWithProjection
 
 from diffusers.models.vae import Decoder
 class BrainNetwork(nn.Module):
@@ -817,3 +823,169 @@ class GNet8_Encoder():
         gnet8j_image_pred = self.gnet8j_predictions(self.stim_data, self._pred_fn, 64, 192, self.joined_checkpoint, mask, batch_size=100, device=self.device)
 
         return torch.from_numpy(gnet8j_image_pred[self.subject])
+
+class IPAdapterPlusXLEmbedder:
+    def __init__(self, image_encoder_path, ip_ckpt, device, num_tokens):
+        self.device = device
+        self.image_encoder_path = image_encoder_path
+        self.ip_ckpt = ip_ckpt
+        self.num_tokens = num_tokens
+        # self.set_ip_adapter()
+
+        # load image encoder
+        self.image_encoder = CLIPVisionModelWithProjection.from_pretrained(self.image_encoder_path).to(
+            self.device, dtype=torch.float16
+        )
+        self.clip_image_processor = CLIPImageProcessor()
+        # image proj model
+        self.image_proj_model = self.init_proj()
+
+        self.load_ip_adapter()
+
+    def init_proj(self):
+        image_proj_model = Resampler(
+            dim=1280,
+            depth=4,
+            dim_head=64,
+            heads=20,
+            num_queries=self.num_tokens,
+            embedding_dim=self.image_encoder.config.hidden_size,
+            output_dim=2048,
+            ff_mult=4,
+        ).to(self.device, dtype=torch.float16)
+        return image_proj_model
+
+    def load_ip_adapter(self):
+        if os.path.splitext(self.ip_ckpt)[-1] == ".safetensors":
+            state_dict = {"image_proj": {}, "ip_adapter": {}}
+            with safe_open(self.ip_ckpt, framework="pt", device="cpu") as f:
+                for key in f.keys():
+                    if key.startswith("image_proj."):
+                        state_dict["image_proj"][key.replace("image_proj.", "")] = f.get_tensor(key)
+                    elif key.startswith("ip_adapter."):
+                        state_dict["ip_adapter"][key.replace("ip_adapter.", "")] = f.get_tensor(key)
+        else:
+            state_dict = torch.load(self.ip_ckpt, map_location="cpu")
+        self.image_proj_model.load_state_dict(state_dict["image_proj"])
+        # ip_layers = torch.nn.ModuleList(self.pipe.unet.attn_processors.values())
+        # ip_layers.load_state_dict(state_dict["ip_adapter"])
+
+    @torch.no_grad()
+    def get_image_embeds(self, pil_image):
+        if isinstance(pil_image, PIL.Image.Image):
+            pil_image = [pil_image]
+        clip_image = self.clip_image_processor(images=pil_image, return_tensors="pt").pixel_values
+        clip_image = clip_image.to(self.device, dtype=torch.float16)
+        clip_image_embeds = self.image_encoder(clip_image, output_hidden_states=True).hidden_states[-2]
+        image_prompt_embeds = self.image_proj_model(clip_image_embeds)
+        return image_prompt_embeds
+
+class IPAdapterPlusXL_LoRA(IPAdapter):
+    """SDXL"""
+
+    def init_proj(self):
+        image_proj_model = Resampler(
+            dim=1280,
+            depth=4,
+            dim_head=64,
+            heads=20,
+            num_queries=self.num_tokens,
+            embedding_dim=self.image_encoder.config.hidden_size,
+            output_dim=self.pipe.unet.config.cross_attention_dim,
+            ff_mult=4,
+        ).to(self.device, dtype=torch.float16)
+        return image_proj_model
+
+    @torch.no_grad()
+    def get_image_embeds(self, pil_image):
+        if isinstance(pil_image, PIL.Image.Image):
+            pil_image = [pil_image]
+        clip_image = self.clip_image_processor(images=pil_image, return_tensors="pt").pixel_values
+        clip_image = clip_image.to(self.device, dtype=torch.float16)
+        clip_image_embeds = self.image_encoder(clip_image, output_hidden_states=True).hidden_states[-2]
+        image_prompt_embeds = self.image_proj_model(clip_image_embeds)
+        uncond_clip_image_embeds = self.image_encoder(
+            torch.zeros_like(clip_image), output_hidden_states=True
+        ).hidden_states[-2]
+        uncond_image_prompt_embeds = self.image_proj_model(uncond_clip_image_embeds)
+        return image_prompt_embeds, uncond_image_prompt_embeds
+
+    def get_uncond_image_embeds(self):
+        pil_image = PIL.Image.new("RGB", (224, 224), (255, 255, 255))
+        clip_image = self.clip_image_processor(images=pil_image, return_tensors="pt").pixel_values
+        clip_image = clip_image.to(self.device, dtype=torch.float16)
+        uncond_clip_image_embeds = self.image_encoder(
+            torch.zeros_like(clip_image), output_hidden_states=True
+        ).hidden_states[-2]
+        uncond_image_prompt_embeds = self.image_proj_model(uncond_clip_image_embeds)
+        return uncond_image_prompt_embeds
+
+    def generate(
+        self,
+        pil_image=None,
+        image_embeds=None,
+        prompt=None,
+        negative_prompt=None,
+        scale=1.0,
+        num_samples=4,
+        seed=None,
+        num_inference_steps=30,
+        **kwargs,
+    ):
+        self.set_scale(scale)
+        if pil_image is not None:
+            num_prompts = 1 if isinstance(pil_image, PIL.Image.Image) else len(pil_image)
+        else:
+            num_prompts = image_embeds.size(0)
+
+        if prompt is None:
+            prompt = "best quality, high quality"
+        if negative_prompt is None:
+            negative_prompt = "monochrome, lowres, bad anatomy, worst quality, low quality"
+
+        if not isinstance(prompt, List):
+            prompt = [prompt] * num_prompts
+        if not isinstance(negative_prompt, List):
+            negative_prompt = [negative_prompt] * num_prompts
+        if pil_image is not None:
+            image_prompt_embeds, uncond_image_prompt_embeds = self.get_image_embeds(pil_image)
+        elif image_embeds is not None:
+            image_prompt_embeds = image_embeds
+            uncond_image_prompt_embeds = self.get_uncond_image_embeds()
+        else:
+            raise ValueError("Either pil_image or image_embeds must be provided")
+            
+        bs_embed, seq_len, _ = image_prompt_embeds.shape
+        image_prompt_embeds = image_prompt_embeds.repeat(1, num_samples, 1)
+        image_prompt_embeds = image_prompt_embeds.view(bs_embed * num_samples, seq_len, -1)
+        uncond_image_prompt_embeds = uncond_image_prompt_embeds.repeat(1, num_samples, 1)
+        uncond_image_prompt_embeds = uncond_image_prompt_embeds.view(bs_embed * num_samples, seq_len, -1)
+
+        with torch.no_grad():
+            (
+                prompt_embeds,
+                negative_prompt_embeds,
+                pooled_prompt_embeds,
+                negative_pooled_prompt_embeds,
+            ) = self.pipe.encode_prompt(
+                prompt,
+                num_images_per_prompt=num_samples,
+                do_classifier_free_guidance=True,
+                negative_prompt=negative_prompt,
+            )
+            prompt_embeds = torch.cat([prompt_embeds, image_prompt_embeds], dim=1)
+            negative_prompt_embeds = torch.cat([negative_prompt_embeds, uncond_image_prompt_embeds], dim=1)
+
+        generator = get_generator(seed, self.device)
+        images = self.pipe(
+            prompt_embeds=prompt_embeds,
+            guidance_scale=0,
+            negative_prompt_embeds=negative_prompt_embeds,
+            pooled_prompt_embeds=pooled_prompt_embeds,
+            negative_pooled_prompt_embeds=negative_pooled_prompt_embeds,
+            num_inference_steps=num_inference_steps,
+            generator=generator,
+            **kwargs,
+        ).images
+
+        return images
