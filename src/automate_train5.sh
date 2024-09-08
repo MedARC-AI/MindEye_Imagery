0c2d713fb33f687d0b6039224a97f402b1c09618
Reese Kneeland <reesekneeland@gmail.com>
2024-09-08 18:11:25 -0500

    Updating training/inference code, automation scripts, figs from first experiments

--- src/automate_train5.sh
+++ src/automate_train5.sh
@@ -14,7 +14,7 @@ subj=1
 # singlesubject finetuning
 model_name="subj0${subj}_40sess_hypatia_vd_dual2"
 echo model_name=${model_name}
-python Train.py --data_path=../dataset --cache_dir=../cache --model_name=${model_name} --no-multi_subject --subj=${subj} --batch_size=${BATCH_SIZE} --max_lr=3e-5 --mixup_pct=.33 --num_epochs=150 --use_prior --prior_scale=30 --clip_scale=1 --blur_scale=.5 --no-use_image_aug --n_blocks=4 --hidden_dim=1024 --num_sessions=40 --ckpt_interval=999 --ckpt_saving --wandb_log #--multisubject_ckpt=../train_logs/${pretrain_model_name} #--no-blurry_recon #--resume_from_ckpt
+# python Train.py --data_path=../dataset --cache_dir=../cache --model_name=${model_name} --no-multi_subject --subj=${subj} --batch_size=${BATCH_SIZE} --max_lr=3e-5 --mixup_pct=.33 --num_epochs=150 --use_prior --prior_scale=30 --clip_scale=1 --blur_scale=.5 --no-use_image_aug --n_blocks=4 --hidden_dim=1024 --num_sessions=40 --ckpt_interval=999 --ckpt_saving --wandb_log #--multisubject_ckpt=../train_logs/${pretrain_model_name} #--no-blurry_recon #--resume_from_ckpt
 
 jupyter nbconvert recon_inference_mi.ipynb --to python
 for mode in "imagery" "vision"; do
