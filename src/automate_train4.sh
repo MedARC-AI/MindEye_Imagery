75efa8c2f6f586590c3877cdd4ff16a9ca45d077
Jordyn Ojeda <jordynaojeda@gmail.com>
2024-09-22 19:31:55 -0500

    updated code

--- src/automate_train4.sh
+++ src/automate_train4.sh
@@ -14,19 +14,19 @@ pretrain_model_name="multisubject_subj0${subj}_hypatia_vd2"
 
 # export BATCH_SIZE=84
 # singlesubject finetuning
-model_name="pretrained_subj0${subj}_40sess_hypatia_vd_snr_0_5"
+model_name="pretrained_subj0${subj}_40sess_hypatia_vd_snr_0_55"
 echo model_name=${model_name}
-python Train.py --data_path=../dataset --cache_dir=../cache --model_name=${model_name} --no-multi_subject --subj=${subj} --batch_size=${BATCH_SIZE} --max_lr=3e-5 --mixup_pct=.33 --num_epochs=150 --use_prior --prior_scale=30 --clip_scale=1 --blur_scale=.5 --no-use_image_aug --n_blocks=4 --hidden_dim=1024 --num_sessions=40 --ckpt_interval=999 --ckpt_saving --wandb_log --multisubject_ckpt=../train_logs/${pretrain_model_name} #--no-blurry_recon #--resume_from_ckpt
+python Train.py --data_path=../dataset --cache_dir=../cache --model_name=${model_name} --no-multi_subject --subj=${subj} --batch_size=${BATCH_SIZE} --max_lr=3e-5 --mixup_pct=.33 --num_epochs=150 --use_prior --prior_scale=30 --clip_scale=1 --blur_scale=.5 --no-use_image_aug --n_blocks=4 --hidden_dim=1024 --num_sessions=40 --ckpt_interval=999 --ckpt_saving --wandb_log --multisubject_ckpt=../train_logs/${pretrain_model_name} --snr_threshold=.55 #--no-blurry_recon #--resume_from_ckpt
 
-jupyter nbconvert recon_inference_mi_vd.ipynb --to python
-for mode in "imagery" "vision"; do
-    python recon_inference_mi.py \
-        --model_name $model_name \
-        --subj $subj \
-        --mode $mode \
-        --cache_dir ../cache \
-        --data_path ../dataset \
-        --hidden_dim 1024 \
-        --n_blocks 4
+# jupyter nbconvert recon_inference_mi_vd.ipynb --to python
+# for mode in "imagery" "vision"; do
+#     python recon_inference_mi.py \
+#         --model_name $model_name \
+#         --subj $subj \
+#         --mode $mode \
+#         --cache_dir ../cache \
+#         --data_path ../dataset \
+#         --hidden_dim 1024 \
+#         --n_blocks 4
 
-    done
+#     done
