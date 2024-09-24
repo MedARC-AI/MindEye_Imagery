1c25be46f65eb3fbe360d0cedd980004c7714143
Reese Kneeland <reesekneeland@gmail.com>
2024-09-24 08:53:31 -0500

    updating automation scripts

--- src/automate_train5.sh
+++ src/automate_train5.sh
@@ -1,20 +1,20 @@
 # Write all the code in jupyter notebook then covert the file.
 jupyter nbconvert Train.ipynb --to python
 export NUM_GPUS=1  # Set to equal gres=gpu:#!
-export BATCH_SIZE=56 # 21 for multisubject / 24 for singlesubject (orig. paper used 42 for multisubject / 24 for singlesubject)
+export BATCH_SIZE=42 # 21 for multisubject / 24 for singlesubject (orig. paper used 42 for multisubject / 24 for singlesubject)
 export GLOBAL_BATCH_SIZE=$((BATCH_SIZE * NUM_GPUS))
-export CUDA_VISIBLE_DEVICES="2" # Set's the GPU device
+export CUDA_VISIBLE_DEVICES="1" # Set's the GPU device
 
 subj=1 
-pretrain_model_name="multisubject_subj0${subj}_hypatia_vd_snr_0_5"
+pretrain_model_name="multisubject_subj0${subj}_hypatia_vd2"
 # echo model_name=${pretrain_model_name}
 #python Train.py --data_path=../dataset --cache_dir=../cache --model_name=${pretrain_model_name} --multi_subject --subj=${subj} --batch_size=${BATCH_SIZE} --max_lr=3e-5 --mixup_pct=.33 --num_epochs=150 --use_prior --prior_scale=30 --clip_scale=1 --blur_scale=.5 --no-use_image_aug --n_blocks=4 --hidden_dim=1024 --num_sessions=40 --ckpt_interval=999 --ckpt_saving --wandb_log --snr_threshold=.50 --resume_from_ckpt #--no-blurry_recon
 
 # export BATCH_SIZE=84
 # singlesubject finetuning
-model_name="pretrained_subj0${subj}_40sess_hypatia_vd_multisubject_snr_0_5"
+model_name="pretrained_subj0${subj}_40sess_hypatia_vd_snr_0_65"
 echo model_name=${model_name}
-python Train.py --data_path=../dataset --cache_dir=../cache --model_name=${model_name} --no-multi_subject --subj=${subj} --batch_size=${BATCH_SIZE} --max_lr=3e-5 --mixup_pct=.33 --num_epochs=150 --use_prior --prior_scale=30 --clip_scale=1 --blur_scale=.5 --no-use_image_aug --n_blocks=4 --hidden_dim=1024 --num_sessions=40 --ckpt_interval=999 --ckpt_saving --wandb_log --multisubject_ckpt=../train_logs/${pretrain_model_name} --snr_threshold=.50 --resume_from_ckpt  #--no-blurry_recon --resume_from_ckpt
+python Train.py --data_path=../dataset --cache_dir=../cache --model_name=${model_name} --no-multi_subject --subj=${subj} --batch_size=${BATCH_SIZE} --max_lr=3e-5 --mixup_pct=.33 --num_epochs=134 --use_prior --prior_scale=30 --clip_scale=1 --blur_scale=.5 --no-use_image_aug --n_blocks=4 --hidden_dim=1024 --num_sessions=40 --ckpt_interval=999 --ckpt_saving --wandb_log --multisubject_ckpt=../train_logs/${pretrain_model_name} --snr_threshold=.65 --resume_from_ckpt  #--no-blurry_recon --resume_from_ckpt
 
 jupyter nbconvert recon_inference_mi.ipynb --to python
 for mode in "imagery" "vision"; do
@@ -26,6 +26,6 @@ for mode in "imagery" "vision"; do
         --data_path ../dataset \
         --hidden_dim 1024 \
         --n_blocks 4 \
-        --snr 0.5
+        --snr 0.65
 
     done
