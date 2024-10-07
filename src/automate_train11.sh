4e4085b4e2dda2a2e9afb8b493888dbd272dae94
Reese Kneeland <reesekneeland@gmail.com>
2024-10-07 16:42:44 -0500

    updating training config

--- src/automate_train11.sh
+++ src/automate_train11.sh
@@ -6,9 +6,9 @@ jupyter nbconvert plots_across_subjects.ipynb --to python
 jupyter nbconvert plots_across_methods.ipynb --to python
 
 export NUM_GPUS=1  # Set to equal gres=gpu:#!
-export BATCH_SIZE=32 # 21 for multisubject / 24 for singlesubject (orig. paper used 42 for multisubject / 24 for singlesubject)
+export BATCH_SIZE=16 # 21 for multisubject / 24 for singlesubject (orig. paper used 42 for multisubject / 24 for singlesubject)
 export GLOBAL_BATCH_SIZE=$((BATCH_SIZE * NUM_GPUS))
-export CUDA_VISIBLE_DEVICES="2"
+export CUDA_VISIBLE_DEVICES="0"
 
 subj=1 
 
