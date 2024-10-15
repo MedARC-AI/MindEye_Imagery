9116d40403ddd86626515134e5df3c2b55f8b151
Reese Kneeland <reesekneeland@gmail.com>
2024-10-15 11:36:01 -0500

    updating inference code

--- src/automate_nsd_imagery_inference.sh
+++ src/automate_nsd_imagery_inference.sh
@@ -4,11 +4,11 @@ jupyter nbconvert final_evaluations_mi_multi.ipynb --to python
 jupyter nbconvert plots_across_subjects.ipynb --to python
 jupyter nbconvert plots_across_methods.ipynb --to python
 jupyter nbconvert Train.ipynb --to python
-export CUDA_VISIBLE_DEVICES="0"
+export CUDA_VISIBLE_DEVICES="3"
 
 for subj in 1; do
-    model_name="pretrained_subj01_40sess_hypatia_vd_dual_proj" 
-    for mode in "vision" "imagery"; do # "vision" "imagery"; do
+    model_name="p_trained_subj01_40sess_hypatia_new_vd_dual_proj" 
+    for mode in "imagery" "vision" ; do # "vision" "imagery"; do
 
         python recon_inference_mi.py \
             --model_name $model_name \
@@ -18,7 +18,9 @@ for subj in 1; do
             --data_path ../dataset \
             --hidden_dim 1024 \
             --n_blocks 4 \
-            --dual_guidance
+            --dual_guidance \
+            # --save_raw \
+            # --raw_path /export/raid1/home/kneel027/Second-Sight/output/mental_imagery_paper_b3/
 
         python final_evaluations_mi_multi.py \
             --model_name $model_name \
@@ -39,11 +41,22 @@ for subj in 1; do
                 --subjs=$subj
     done
 
+    
+
 
     python plots_across_methods.py \
-        --methods "mindeye1_subj01,final_subj01_pretrained_40sess_24bs,pretrained_subj01_40sess_hypatia_vd2,pretrained_subj01_40sess_hypatia_vd_dual_proj,pretrained_subj01_40sess_hypatia_vd_snr_0_5,pretrained_subj01_40sess_hypatia_vd_snr_0_55" \
+        --methods "mindeye1_subj01, \
+        braindiffuser_subj01, \
+        final_subj01_pretrained_40sess_24bs, \
+        pretrained_subj01_40sess_hypatia_vd2, \
+        subj01_40sess_hypatia_turbo_ridge_flat3, \
+        pretrained_subj01_40sess_hypatia_vd_dual_proj_avg, \
+        p_trained_subj01_40sess_hypatia_new_vd_dual_proj_normalized_preds, \
+        p_trained_subj01_40sess_hypatia_new_vd_dual_proj \
+        subj01_40sess_hypatia_ridge_flat_dp5"  \
         --data_path ../dataset \
-        --output_path ../figs/
+        --output_path ../figs/ \
+        --output_file methods_scatter_reduced_new_vd
 
 done
 
