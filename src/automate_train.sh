1a486bcac23b33ef8ab8e1add0838f0b0caeee2f
Reese Kneeland <reesekneeland@gmail.com>
2024-10-24 21:09:08 -0500

    removing messy files, refactoring train and recon scripts

--- src/automate_train.sh
+++ src/automate_train.sh
@@ -1,77 +1,41 @@
-jupyter nbconvert Train.ipynb --to python
-jupyter nbconvert recon_inference_mi.ipynb --to python
+
+jupyter nbconvert Train_ridge_cascade_sd35.ipynb --to python
+jupyter nbconvert recon_inference_mi_ridge_cascade.ipynb --to python
+jupyter nbconvert enhanced_recon_inference_mi_ridge.ipynb --to python
 jupyter nbconvert final_evaluations_mi_multi.ipynb --to python
 jupyter nbconvert plots_across_subjects.ipynb --to python
 jupyter nbconvert plots_across_methods.ipynb --to python
 
 export NUM_GPUS=1  # Set to equal gres=gpu:#!
-export BATCH_SIZE=42 # 21 for multisubject / 24 for singlesubject (orig. paper used 42 for multisubject / 24 for singlesubject)
+export BATCH_SIZE=50 # 21 for multisubject / 24 for singlesubject (orig. paper used 42 for multisubject / 24 for singlesubject)
 export GLOBAL_BATCH_SIZE=$((BATCH_SIZE * NUM_GPUS))
-export CUDA_VISIBLE_DEVICES="2"
+export CUDA_VISIBLE_DEVICES="1"
 
 subj=1 
-pretrain_model_name="multisubject_subj0${subj}_hypatia_vd_dual_proj"
-echo model_name=${pretrain_model_name}
-python Train.py \
-    --data_path=../dataset \
-    --cache_dir=../cache \
-    --model_name=${pretrain_model_name} \
-    --multi_subject --subj=${subj} \
-    --batch_size=${BATCH_SIZE} \
-    --max_lr=3e-5 \
-    --mixup_pct=.33 \
-    --num_epochs=150 \
-    --use_prior \
-    --prior_scale=30 \
-    --clip_scale=1 \
-    --blur_scale=.5 \
-    --no-use_image_aug \
-    --n_blocks=4 \
-    --hidden_dim=1024 \
-    --num_sessions=40 \
-    --ckpt_interval=999 \
-    --ckpt_saving \
-    --wandb_log
 
-# singlesubject finetuning
-
-model_name="pretrained_subj0${subj}_40sess_hypatia_vd_dual_proj"
+model_name="subj01_40sess_hypatia_ridge_sc_sd35"
 echo model_name=${model_name}
-python Train.py --data_path=../dataset \
-    --cache_dir=../cache \
-    --model_name=${model_name} \
-    --no-multi_subject \
-    --subj=${subj} \
-    --batch_size=${BATCH_SIZE} \
-    --max_lr=3e-5 \
-    --mixup_pct=.33 \
-    --num_epochs=150 \
-    --use_prior \
-    --prior_scale=30 \
-    --clip_scale=1 \
-    --blur_scale=.5 \
-    --no-use_image_aug \
-    --n_blocks=4 \
-    --hidden_dim=1024 \
-    --num_sessions=40 \
-    --ckpt_interval=999 \
-    --ckpt_saving \
-    --wandb_log \
-    --multisubject_ckpt=../train_logs/${pretrain_model_name} \
-    --dual_guidance 
 
+# python Train_ridge_cascade_sd35.py \
+#     --data_path=../dataset \
+#     --cache_dir=../cache \
+#     --model_name=${model_name} \
+#     --no-multi_subject \
+#     --subj=${subj} \
+#     --batch_size=${BATCH_SIZE} \
+#     --weight_decay=60000
 
-for mode in "imagery" "vision"; do
-    python recon_inference_mi.py \
-        --model_name $model_name \
-        --subj $subj \
-        --mode $mode \
-        --cache_dir ../cache \
-        --data_path ../dataset \
-        --hidden_dim 1024 \
-        --n_blocks 4 \
-        --dual_guidance
+for mode in "vision" "imagery"; do
 
+    # python recon_inference_mi_ridge_cascade.py \
+    #     --model_name $model_name \
+    #     --subj $subj \
+    #     --mode $mode \
+    #     --cache_dir ../cache \
+    #     --data_path ../dataset \
+    #     --save_raw \
+    #     --raw_path /export/raid1/home/kneel027/Second-Sight/output/mental_imagery_paper_b3/ 
+        
     python final_evaluations_mi_multi.py \
             --model_name $model_name \
             --all_recons_path evals/${model_name}/${model_name}_all_recons_${mode}.pt \
@@ -79,6 +43,7 @@ for mode in "imagery" "vision"; do
             --mode $mode \
             --data_path ../dataset \
             --cache_dir ../cache
+            # --no-blurry_recon
 
     python plots_across_subjects.py \
             --model_name="${model_name}" \
@@ -89,5 +54,48 @@ for mode in "imagery" "vision"; do
             --all_recons_path evals/${model_name}/${model_name}_all_recons_${mode}.pt \
             --subjs=$subj
 
+    python enhanced_recon_inference_mi_ridge.py \
+        --model_name $model_name \
+        --subj $subj \
+        --mode $mode \
+        --cache_dir ../cache \
+        --data_path ../dataset \
+        --save_raw \
+        --raw_path /export/raid1/home/kneel027/Second-Sight/output/mental_imagery_paper_b3/  \
+        --sd35
+        
+    python final_evaluations_mi_multi.py \
+            --model_name "${model_name}_enhanced" \
+            --all_recons_path evals/${model_name}/${model_name}_all_recons_${mode}.pt \
+            --subj $subj \
+            --mode $mode \
+            --data_path ../dataset \
+            --cache_dir ../cache
+            # --no-blurry_recon
+
+    python plots_across_subjects.py \
+            --model_name="${model_name}_enhanced" \
+            --mode="${mode}" \
+            --data_path ../dataset \
+            --cache_dir ../cache \
+            --criteria all \
+            --all_recons_path evals/${model_name}/${model_name}_all_recons_${mode}.pt \
+            --subjs=$subj
+
     done
 
+python plots_across_methods.py \
+    --methods "mindeye1_subj01, \
+    braindiffuser_subj01, \
+    final_subj01_pretrained_40sess_24bs, \
+    pretrained_subj01_40sess_hypatia_vd_dual_proj_avg, \
+    subj01_40sess_hypatia_turbo_ridge_flat3, \
+    subj01_40sess_hypatia_ridge_sc3,
+    subj01_40sess_hypatia_ridge_sc_flux,
+    subj01_40sess_hypatia_ridge_sc_flux_enhanced,
+    subj01_40sess_hypatia_ridge_sc_sd35,
+    subj01_40sess_hypatia_ridge_sc_sd35_enhanced" \
+    --data_path ../dataset \
+    --output_path ../figs/ \
+    --output_file methods_scatter_reduced4
+    
