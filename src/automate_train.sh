d489770103fb28b566d5a110e58a2fec34cd7282
Reese Kneeland <reesekneeland@gmail.com>
2024-10-24 19:53:44 -0500

    Adding code to do recons with only SD3.5

--- src/automate_train.sh
+++ src/automate_train.sh
@@ -4,33 +4,32 @@ jupyter nbconvert final_evaluations_mi_multi.ipynb --to python
 jupyter nbconvert plots_across_subjects.ipynb --to python
 jupyter nbconvert plots_across_methods.ipynb --to python
 
-export CUDA_VISIBLE_DEVICES="0"
+export CUDA_VISIBLE_DEVICES="1"
 
 subj=1 
 
-model_name="subj01_40sess_hypatia_ridge_sc3"
+model_name="subj01_40sess_hypatia_ridge_sd35"
 echo model_name=${model_name}
 
-# python Train.py \
-#     --data_path=../dataset \
-#     --cache_dir=../cache \
-#     --model_name=${model_name} \
-#     --no-multi_subject \
-#     --subj=${subj} \
-#     --weight_decay=60000 \
-#     --dual_guidance
+python Train.py \
+    --data_path=../dataset \
+    --cache_dir=../cache \
+    --model_name=${model_name} \
+    --no-multi_subject \
+    --subj=${subj} \
+    --weight_decay=60000
 
 for mode in "vision" "imagery"; do
 
-    # python recon_inference_mi.py \
-    #     --model_name $model_name \
-    #     --subj $subj \
-    #     --mode $mode \
-    #     --cache_dir ../cache \
-    #     --data_path ../dataset \
-    #     --save_raw \
-    #     --raw_path /export/raid1/home/kneel027/Second-Sight/output/mental_imagery_paper_b3/ \
-    #     --dual_guidance
+    python recon_inference_mi.py \
+        --model_name $model_name \
+        --subj $subj \
+        --mode $mode \
+        --cache_dir ../cache \
+        --data_path ../dataset \
+        --save_raw \
+        --raw_path /export/raid1/home/kneel027/Second-Sight/output/mental_imagery_paper_b3/ \
+        --dual_guidance
         
     python final_evaluations_mi_multi.py \
             --model_name $model_name \
@@ -39,16 +38,15 @@ for mode in "vision" "imagery"; do
             --mode $mode \
             --data_path ../dataset \
             --cache_dir ../cache
-            # --no-blurry_recon
 
-    # python plots_across_subjects.py \
-    #         --model_name="${model_name}" \
-    #         --mode="${mode}" \
-    #         --data_path ../dataset \
-    #         --cache_dir ../cache \
-    #         --criteria all \
-    #         --all_recons_path evals/${model_name}/${model_name}_all_recons_${mode}.pt \
-    #         --subjs=$subj
+    python plots_across_subjects.py \
+            --model_name="${model_name}" \
+            --mode="${mode}" \
+            --data_path ../dataset \
+            --cache_dir ../cache \
+            --criteria all \
+            --all_recons_path evals/${model_name}/${model_name}_all_recons_${mode}.pt \
+            --subjs=$subj
 
     done
 
@@ -56,12 +54,14 @@ python plots_across_methods.py \
     --methods "mindeye1_subj01, \
     braindiffuser_subj01, \
     final_subj01_pretrained_40sess_24bs, \
-    pretrained_subj01_40sess_hypatia_vd2, \
     pretrained_subj01_40sess_hypatia_vd_dual_proj_avg, \
     subj01_40sess_hypatia_turbo_ridge_flat3, \
-    subj01_40sess_hypatia_ridge_flat_dp5, \
-    subj01_40sess_hypatia_ridge_rank_order_rois_13, \
-    subj01_40sess_hypatia_ridge_sc3" \
+    subj01_40sess_hypatia_ridge_sc3,
+    subj01_40sess_hypatia_ridge_sc_flux,
+    subj01_40sess_hypatia_ridge_sc_flux_enhanced,
+    subj01_40sess_hypatia_ridge_sc_sd35,
+    subj01_40sess_hypatia_ridge_sc_sd35_enhanced,
+    subj01_40sess_hypatia_ridge_sd35" \
     --data_path ../dataset \
     --output_path ../figs/ \
-    --output_file methods_scatter_reduced3
+    --output_file methods_scatter_reduced4
