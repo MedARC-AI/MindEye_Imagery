e689b9d57bf33188acfd5e555fdca35ce14c9e1d
Reese Kneeland <reesekneeland@gmail.com>
2024-11-01 08:29:43 -0500

    adding temp file to fill in the gaps

--- src/automate_best4.sh
+++ src/automate_best4.sh
@@ -1,62 +1,62 @@
-jupyter nbconvert Train.ipynb --to python
-jupyter nbconvert recon_inference_mi.ipynb --to python
+# jupyter nbconvert Train.ipynb --to python
+jupyter nbconvert recon_inference_mi_temp.ipynb --to python
 jupyter nbconvert final_evaluations_mi_multi.ipynb --to python
 jupyter nbconvert plots_across_subjects.ipynb --to python
 jupyter nbconvert plots_across_methods.ipynb --to python
 
 export CUDA_VISIBLE_DEVICES="3"
-for subj in 7; do
-    model_name="subj0${subj}_40sess_hypatia_mirage2"
+# for subj in 7; do
+#     model_name="subj0${subj}_40sess_hypatia_mirage2"
 
-    python Train.py \
-        --data_path=../dataset \
-        --cache_dir=../cache \
-        --model_name=${model_name} \
-        --subj=${subj} 
+#     python Train.py \
+#         --data_path=../dataset \
+#         --cache_dir=../cache \
+#         --model_name=${model_name} \
+#         --subj=${subj} 
 
-    for mode in "vision" "imagery"; do # "shared1000"
+#     for mode in "vision" "imagery"; do # "shared1000"
 
-        python recon_inference_mi.py \
-            --model_name $model_name \
-            --subj $subj \
-            --mode $mode \
-            --cache_dir ../cache \
-            --data_path ../dataset \
-            --save_raw \
-            --raw_path /export/raid1/home/kneel027/Second-Sight/output/mental_imagery_paper_b3/ 
+#         python recon_inference_mi.py \
+#             --model_name $model_name \
+#             --subj $subj \
+#             --mode $mode \
+#             --cache_dir ../cache \
+#             --data_path ../dataset \
+#             --save_raw \
+#             --raw_path /export/raid1/home/kneel027/Second-Sight/output/mental_imagery_paper_b3/ 
             
-        python final_evaluations_mi_multi.py \
-                --model_name $model_name \
-                --all_recons_path evals/${model_name}/${model_name}_all_recons_${mode}.pt \
-                --subj $subj \
-                --mode $mode \
-                --data_path ../dataset \
-                --cache_dir ../cache
+#         python final_evaluations_mi_multi.py \
+#                 --model_name $model_name \
+#                 --all_recons_path evals/${model_name}/${model_name}_all_recons_${mode}.pt \
+#                 --subj $subj \
+#                 --mode $mode \
+#                 --data_path ../dataset \
+#                 --cache_dir ../cache
 
-        python plots_across_subjects.py \
-                --model_name="${model_name}" \
-                --mode="${mode}" \
-                --data_path ../dataset \
-                --cache_dir ../cache \
-                --criteria all \
-                --all_recons_path evals/${model_name}/${model_name}_all_recons_${mode}.pt \
-                --subjs=$subj
+#         python plots_across_subjects.py \
+#                 --model_name="${model_name}" \
+#                 --mode="${mode}" \
+#                 --data_path ../dataset \
+#                 --cache_dir ../cache \
+#                 --criteria all \
+#                 --all_recons_path evals/${model_name}/${model_name}_all_recons_${mode}.pt \
+#                 --subjs=$subj
 
-        done
-    done
+#         done
+#     done
 
 for subj in 1 2 5 7; do
     model_name="subj0${subj}_40sess_hypatia_mirage3"
 
-    python Train.py \
-        --data_path=../dataset \
-        --cache_dir=../cache \
-        --model_name=${model_name} \
-        --subj=${subj} 
+    # python Train.py \
+    #     --data_path=../dataset \
+    #     --cache_dir=../cache \
+    #     --model_name=${model_name} \
+    #     --subj=${subj} 
 
-    for mode in "vision" "imagery"; do # "shared1000"
+    for mode in "imagery"; do # "shared1000""vision" 
 
-        python recon_inference_mi.py \
+        python recon_inference_mi_temp.py \
             --model_name $model_name \
             --subj $subj \
             --mode $mode \
