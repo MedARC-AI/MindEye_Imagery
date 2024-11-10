35b3275263e460140f5abb7a736cbb0e41cf63a9
Jonathan Xu <jonathanxu25@gmail.com>
2024-11-10 20:00:31 +0000

    adding retrieval to unclip

--- src/generative_models/sgm/util.py
+++ src/generative_models/sgm/util.py
@@ -177,12 +177,20 @@ def instantiate_from_config(config):
 
 def get_obj_from_str(string, reload=False, invalidate_cache=True):
     module, cls = string.rsplit(".", 1)
+    if module.startswith("sgm"):
+        module = "generative_models." + module
     if invalidate_cache:
         importlib.invalidate_caches()
     if reload:
         module_imp = importlib.import_module(module)
         importlib.reload(module_imp)
-    return getattr(importlib.import_module(module, package=None), cls)
+    try:
+        attributes = getattr(importlib.import_module(module, package=None), cls)
+    except:
+        import pdb
+        pdb.set_trace()
+
+    return attributes
 
 
 def append_zero(x):
