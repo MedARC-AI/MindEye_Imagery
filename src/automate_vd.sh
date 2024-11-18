0e1a562988f9630bf13415aa9d8378673131f004
Reese Kneeland <reesekneeland@gmail.com>
2024-11-18 10:24:24 -0600

    cleaning up branch to run experiments

--- src/automate_vd.sh
+++ src/automate_vd.sh
@@ -1,23 +1,17 @@
-jupyter nbconvert Train_vd.ipynb --to python
-jupyter nbconvert recon_inference_mi_vd.ipynb --to python
+jupyter nbconvert Train.ipynb --to python
+jupyter nbconvert recon_inference_mi.ipynb --to python
 jupyter nbconvert final_evaluations_mi_multi.ipynb --to python
 jupyter nbconvert plots_across_subjects.ipynb --to python
 jupyter nbconvert plots_across_methods.ipynb --to python
 
 export CUDA_VISIBLE_DEVICES="1"
 for subj in 1; do
-    model_name="subj0${subj}_40sess_hypatia_mirage_vd3"
+    model_name="subj0${subj}_40sess_hypatia_mirage_vd_ts1"
 
-    # python Train_vd.py \
-    #     --data_path=../dataset \
-    #     --cache_dir=../cache \
-    #     --model_name=${model_name} \
-    #     --subj=${subj} \
-    #     --no-prompt_recon
-    # cp -r ../train_logs/subj0${subj}_40sess_hypatia_mirage_vd ../train_logs/${model_name}
+    cp -r ../train_logs/subj0${subj}_40sess_hypatia_mirage_vd ../train_logs/${model_name}
     for mode in "vision"; do #"shared1000"  "imagery"
 
-        python recon_inference_mi_vd.py \
+        python recon_inference_mi.py \
             --model_name $model_name \
             --subj $subj \
             --mode $mode \
@@ -26,9 +20,11 @@ for subj in 1; do
             --save_raw \
             --raw_path /export/raid1/home/kneel027/Second-Sight/output/mental_imagery_paper_b3/ \
             --no-prompt_recon \
-            --strength 0.85 \
+            --ts 1 \
+            --no-retrieval \
             --no-filter_contrast \
-            --no-filter_sharpness
+            --no-filter_sharpness \
+            --strength 0.85 \
             
         python final_evaluations_mi_multi.py \
                 --model_name $model_name \
